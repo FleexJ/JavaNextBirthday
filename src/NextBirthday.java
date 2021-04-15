573b3c20932b7249db1a120d0224d644d3a6c044
kreslojerry <52152485+kreslojerry@users.noreply.github.com>
2021-04-15 17:39:30 +0500

    Рефакторинг

--- src/NextBirthday.java
+++ src/NextBirthday.java
@@ -5,15 +5,13 @@ public class NextBirthday {
 
     public static void calculateNextBirthday() {
         Calendar dateBirth = Calendar.getInstance();
-        int day, month, year;
-
         Scanner scanner = new Scanner(System.in);
+
         //Пока не считаем корректную дату
         while (true) {
             //Считывание даты
             System.out.print("\nEnter your date of birth: ");
             String input = scanner.nextLine();
-            Calendar now = Calendar.getInstance();
 
             //Если введенная дата не подходит под формат
             if (!input.matches("^[0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{1,4}$")) {
@@ -22,9 +20,10 @@ public class NextBirthday {
             //Иначе парсим дату из строки и запоминаем
             else {
                 String[] inputSplit = input.split("\\.");
-                day = Integer.parseInt(inputSplit[0]);
-                month = Integer.parseInt(inputSplit[1]) - 1;
-                year = Integer.parseInt(inputSplit[2]);
+                int day = Integer.parseInt(inputSplit[0]);
+                int month = Integer.parseInt(inputSplit[1]) - 1;
+                int year = Integer.parseInt(inputSplit[2]);
+
                 if (month > 11 || month < 0) {
                     System.out.println("Incorrect date!");
                     continue;
@@ -32,16 +31,17 @@ public class NextBirthday {
 
                 dateBirth.set(year, month, day, 0, 0, 0);
                 //Устанавливаем текущий год
+                Calendar now = Calendar.getInstance();
                 dateBirth.set(Calendar.YEAR, now.get(Calendar.YEAR));
                 //Если день уже прошел, то прибавляем год
                 if (dateBirth.before(now)) {
                     dateBirth.add(Calendar.YEAR, 1);
                 }
-                System.out.println("\nNext birthday:\n" + dateBirth.getTime());
                 break;
             }
         }
 
+        System.out.println("\nNext birthday:\n" + dateBirth.getTime());
         //Вычисляем разницу между следующим днем рождения и текущей датой в миллисекундах, переводим миллисекунды в дни
         double nextDays = (dateBirth.getTimeInMillis() - Calendar.getInstance().getTimeInMillis()) / 86400000.0;
         System.out.println("\nDays left until birthday:\n" + nextDays);
